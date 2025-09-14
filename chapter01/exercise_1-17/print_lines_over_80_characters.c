79d52db12f8c0c4423ca72bd7f4d985686f01568
DylanO9 <dylan.olivares11@gmail.com>
2025-09-14 16:52:54 -0700

    feat(exercises): add solution for exercise 17 for chapter 1

---
+++ chapter01/exercise_1-17/print_lines_over_80_characters.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LIMIT 100
+
+int my_getline(char s[], int limit);
+
+int main() {
+	char line[MAX_LIMIT];
+	int length;
+
+	while ((length = my_getline(line, MAX_LIMIT)) > 0) {
+		if (length > 80) {
+			printf("%s", line);
+		}
+	}
+
+	return 0;
+}
+
+int my_getline(char s[], int limit) {
+	/* 
+		Strategy:
+		Use c to store the current character and place it within s[i] until we find a newline character
+		which we will then place it at this index
+ 		and add a null character to the end of the array to make sure we have this determinant
+	*/
+	int c, i;
+	for (i = 0; (c = getchar()) != EOF && i < limit - 1 && c != '\n'; i++) {
+		s[i] = c;
+	}	
+	// if it exited because of a newline character, then we must check this. Do we have to check that s has one more spot to place '\n'
+	if (c == '\n') {
+		// Place this newline into the array
+		s[i] = c;	
+		i++;
+	}	
+	
+	// We need to place a null character at the end as well
+	s[i] = '\0';
+
+	return i;
+}
+
+
