cc86104a81cc9676f49b33e949cb07a92f99c1e7
DylanO9 <dylan.olivares11@gmail.com>
2025-10-02 03:48:56 +0000

    feat(exercises): add solution for exercise 1 in chapter 3 for writing a program that has a binary search implementation with only one if condition instead of two

---
+++ chapter03/exercise_3-1/binary_search.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int binsearch(int target, int array[], int length);
+
+int main() {
+    int test_arr[5] = {1,3,6,8,9};
+    int target = 6;
+    int n = 5; 
+    
+    printf("Found Target At Index: %d\n", binsearch(target, test_arr, n));
+}
+
+int binsearch(int x, int v[], int n)
+{
+    int low, high, mid, saved_index;
+    
+    low = 0;
+    high = n - 1;
+    saved_index = -1;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (x <= v[mid]) {
+            saved_index = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }        
+    }
+    return saved_index;
+}
