44454eda8ed1c2542150b3cdd2e04aac88c262ab
DylanO9 <dylan.olivares11@gmail.com>
2025-10-14 01:03:51 +0000

    feat(exercises): add solution for exercise 8 in chapter 5 to add error checking to day_of_year and month_day

---
+++ chapter05/exercise_5-8/calendarConversions.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+static char daytab[2][13] = {
+    {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+    {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+int day_of_year(int year, int month, int day);
+void month_day(int year, int yearday, int *month, int *pday);
+
+int main()
+{
+    int d = day_of_year(2019, 5, 20);
+    printf("days: %d\n", d); 
+}
+
+int day_of_year(int year, int month, int day)
+{
+    // What if month, or day are out of bounds?
+    if (month < 1 || month > 12 || year < 0)
+        return -1;
+    int i, leap;
+    leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    if (day < 1 || day > daytab[leap][month])
+        return -1; 
+    for (i = 1; i < month; i++)
+        day += daytab[leap][i];
+    return day;
+}
+
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+    int i, leap;
+    leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    if (year < 0) {
+       *pmonth = -1;
+        *pday = -1; 
+        return;
+    }
+    int max_days = 0;
+    for (int j = 1; j < 13; j++) {
+        max_days += daytab[leap][j];
+    } 
+    if (yearday < 1 || yearday > max_days) {
+        *pmonth = -1;
+        *pday = -1;
+        return;
+    }
+    for (i = 1; yearday > daytab[leap][i]; i++)
+        yearday -= daytab[leap][i];
+    *pmonth = i;
+    *pday = yearday;
+}
