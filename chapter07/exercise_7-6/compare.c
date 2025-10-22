4687ed186128de9c106ad98c900e3312a51b84e6
DylanO9 <dylan.olivares11@gmail.com>
2025-10-22 01:49:00 +0000

    feat(exercises): add proper closing for exercise 6 in chapter 7

--- chapter07/exercise_7-6/compare.c
+++ chapter07/exercise_7-6/compare.c
@@ -20,20 +20,25 @@ int main(int argc, char *argv[]) {
 
     if ((file_1 = fopen(*(argv + 1), "r")) == NULL) {
         fprintf(stderr, "There was a problem opening the first file\n");
+        return 1;
     }
     if ((file_2 = fopen(*(argv + 2), "r")) == NULL) {
         fprintf(stderr, "There was a problem opening the second file\n");
+        return 1;
     }
 
     while (fgets(line_1, MAX_CHARS, file_1) != NULL && fgets(line_2, MAX_CHARS, file_2) != NULL) {
-        int compare;
-        if ((compare = strcmp(line_1, line_2)) != 0) {
-            printf("Compare value %d\n", compare);
+        if (strcmp(line_1, line_2) != 0) {
             printf("Line 1: %s\n", line_1);
             printf("Line 2: %s\n", line_2);
+            fclose(file_1);
+            fclose(file_2);
             return 0;
         }
     }
+
+    fclose(file_1);
+    fclose(file_2);
     fprintf(stderr, "One of the files reached the end before two different lines were found\n");
     return 0; 
 }
