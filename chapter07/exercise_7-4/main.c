ad36a0b7844b9f8fe951c440bf203dde3501e502
DylanO9 <dylan.olivares11@gmail.com>
2025-10-27 08:03:29 +0000

    feat(exercises): add modification for exercise 4 in chapter 7, so we are not wrapping around scanf and are processing the digit manually

--- chapter07/exercise_7-4/main.c
+++ chapter07/exercise_7-4/main.c
@@ -39,7 +39,23 @@ int my_scanf(char *format, ...) {
         } 
         switch(*(++fp)) {
             case 'd':
-                scanf("%d", va_arg(ap, int *));
+                // Read digit
+                // How do you read a digit from a string and convert it to an actual digit?
+                while (isspace(c = getchar()))
+                    ;
+                int sign = 1;
+                int digit = 0;
+                if (c == '-') {
+                    sign = -1;
+                    c = getchar();
+                }
+                if (!isdigit(c)) return 0;
+                do {
+                    digit = digit * 10 + (c - '0');
+                } while (isdigit(c = getchar()));
+                ungetc(c, stdin); 
+                int *format_d = va_arg(ap, int *);
+                *format_d = digit; // Store our value into the val
                 break;
             case 's':
                 scanf("%s", va_arg(ap, char *));
